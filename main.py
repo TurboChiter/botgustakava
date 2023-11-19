7f791609d5752624dd59e28bf8f34d918d4ed940
TurboChiter <91634570+TurboChiter@users.noreply.github.com>
2023-11-19 14:43:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -141,9 +141,9 @@ async def message(message: types.Message):
 	#-----------
 
 	elif db.check(userid):
-        state = db.getstate(userid)
-        if text == "Выгрузить базу":
-            updater(userid)
+		state = db.getstate(userid)
+		if text == "Выгрузить базу":
+			updater(userid)
 		if text == "Назад":
 			#state = db.getstate(userid)
 			#if state == 3:
